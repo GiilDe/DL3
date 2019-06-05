018ce6b2a7fb06df59248cc938b7c648ff9ed35b
Didi <adihorowitz@campus.technion.ac.il>
2019-06-05 10:16:29 +0300

    training part 3

--- hw3/answers.py
+++ hw3/answers.py
@@ -132,14 +132,18 @@ def part3_gan_hyperparams():
     )
     # TODO: Tweak the hyperparameters to train your GAN.
     # ====== YOUR CODE: ======
-    hypers['batch_size'] = 128
+    hypers['batch_size'] = 32
     hypers['z_dim'] = 10
-    hypers['data_label'] = 0
-    hypers['label_noise'] = 0.1
-    hypers['discriminator_optimizer']['type'] = "Adam"
-    hypers['discriminator_optimizer']['lr'] = 0.001
-    hypers['generator_optimizer']['type'] = "Adam"
-    hypers['generator_optimizer']['lr'] = 0.001
+    hypers['data_label'] = 1
+    hypers['label_noise'] = 0.3
+    hypers['discriminator_optimizer']['type']='Adam'
+    hypers['discriminator_optimizer']['weight_decay']=0.002
+    hypers['discriminator_optimizer']['betas']=(0.5,0.999)
+    hypers['discriminator_optimizer']['lr']=0.0002
+    hypers['generator_optimizer']['type']='Adam'
+    hypers['generator_optimizer']['weight_decay']=0.002
+    hypers['generator_optimizer']['betas']=(0.4,0.999)
+    hypers['generator_optimizer']['lr']=0.0002
 
 
     # ========================
